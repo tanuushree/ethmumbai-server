77a423debf401c9ffbc2d4a1cb5ab634ad7ed12d
Tanu Shree <tanushree.wrk@gmail.com>
2025-10-11 19:45:47 +0530

    chore(app): update package.json and dependencies in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { CartModule } from './cart/cart.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule, 
+    CartModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
